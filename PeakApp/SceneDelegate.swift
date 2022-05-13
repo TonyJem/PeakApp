18bb52ef0aa68813059633cf2b0f5d9082bb21b9
TonyJem <a.jemanov@gmail.com>
2022-05-13 20:09:46 +0300

    Initial commit

--- PeakApp/SceneDelegate.swift
+++ PeakApp/SceneDelegate.swift
@@ -1,10 +1,3 @@
-//
-//  SceneDelegate.swift
-//  PeakApp
-//
-//  Created by Office-iMac on 2022-05-13.
-//
-
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
@@ -46,7 +39,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
-
--- PeakApp/ViewController.swift
+++ PeakApp/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  PeakApp
-//
-//  Created by Office-iMac on 2022-05-13.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -13,7 +6,4 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
