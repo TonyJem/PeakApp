f1a549732b6d0d4f3f3d025e48d1b2b7543279ce
TonyJem <a.jemanov@gmail.com>
2022-05-14 11:59:04 +0300

    Update WelcomeViewController.swift

--- PeakApp/Controllers/WelcomeViewController.swift
+++ PeakApp/Controllers/WelcomeViewController.swift
@@ -1,10 +1,56 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
+    
+    // MARK: - Views
+    private lazy var enterButton: UIButton = {
+        let button = UIButton(type: .system)
+        
+        let title = "Войти"
+        button.setTitle(title, for: .normal)
+        
+        button.backgroundColor = .blue
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(didTapEnterButton), for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .cyan
+        
+        setupViews()
+        setConstraints()
+    }
+    
+    // MARK: - Actions
+    @objc private func didTapEnterButton() {
+        print("🟢 didTapEnterButton in LoginViewController")
+        
+        let mainVC = MainViewController()
+        mainVC.modalPresentationStyle = .fullScreen
+        present(mainVC, animated: true)
+    }
+    
+    // MARK: - Private Methods
+    private func setupViews() {
+        view.addSubview(enterButton)
     }
 }
+
+// MARK: - SetConstraints
+extension WelcomeViewController {
+    private func setConstraints() {
+        
+        NSLayoutConstraint.activate([
+            enterButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            enterButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100),
+            enterButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            enterButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+}
+
