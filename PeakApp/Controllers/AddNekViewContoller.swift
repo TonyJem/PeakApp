8e37b095f154946d13b7e55060ec00db32a13d93
TonyJem <a.jemanov@gmail.com>
2022-05-14 11:49:46 +0300

    Create AddNekViewContoller template

---
+++ PeakApp/Controllers/AddNekViewContoller.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class AddNekViewContoller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+}
